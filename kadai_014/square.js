fc58e8e4e1d515d95ca708e69631591a793c767d
htakahashi-kongo <htakahashi@kongomail.com>
2024-07-06 16:18:48 +0900

    課題提出

---
+++ kadai_014/square.js
@@ -0,0 +1,18 @@
+let i = Math.floor(Math.random() * 100);
+console.log(i);
+
+const square = (num) => {
+    return num ** 2;
+}
+
+console.log(square(i))
+
+// --------------------------------------------------
+
+function sq (j) {
+    a = j ** 2;
+    return a;
+}
+
+sq(i);
+console.log(a);
